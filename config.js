8afc1afc49db3676191f2e32afe045a0e446ce09
npttruong03 <106502107+npttruong03@users.noreply.github.com>
2023-09-25 00:24:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const nameGirl = 'Em';
-const giftUrl = 'http://nodemy.vn';
+const giftUrl = 'https://www.facebook.com/truong.seven.9235';
 const eventName = 'Chúc Mừng Trung thu';
 const titleCard = 'Tặng Em';
 const contentCard = 'Ngày Trung thu vui vẻ. Chúc em có một tương lai tươi sáng và thành công. Có em trong đời, anh luôn cảm thấy may mắn và hạnh phúc, cuộc sống ý nghĩa, vui vẻ hơn. Anh yêu Em rất nhiều!';
