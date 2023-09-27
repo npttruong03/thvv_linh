89dc0a6ca3b5d486d1661a278a58f90c28ff52e3
npttruong03 <106502107+npttruong03@users.noreply.github.com>
2023-09-27 21:15:06 +0700

    lại

---
+++ thvv_linh-main/config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/truong.seven.9235';
+const eventName = 'Chúc Mừng Trung thu';
+const titleCard = 'Tặng Em';
+const contentCard = 'Ngày Trung thu vui vẻ. Chúc em có một tương lai tươi sáng và thành công. Có em trong đời, anh luôn cảm thấy may mắn và hạnh phúc, cuộc sống ý nghĩa, vui vẻ hơn. Anh yêu Em rất nhiều!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'dth.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
